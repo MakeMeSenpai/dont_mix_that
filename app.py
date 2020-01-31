573cc25233e8fbffcf57ccaa7bcec159afa262f6
Anthony <sean.a.protho1@gmail.com>
2020-01-31 15:45:19 -0800

    small fix

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__, static_url_path="")
 
 @app.route("/")
 def index():
-    return render_template('index.html'))
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debugger=True)
-    
