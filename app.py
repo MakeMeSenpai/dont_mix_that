fd61af9614aa30136f1d6d04eb42f1853dc3996c
liyaSileshi <liyasileshi@gmail.com>
2020-02-03 14:11:02 -0800

    added sidebar

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-from jinja2 import Environment, PackageLoader, select_autoescape
-from flask import Flask, render_template
-
-app = Flask(__name__, static_url_path="")
-
-@app.route("/")
-def index():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debugger=True)
-=======
 from flask import Flask, render_template, request, redirect, url_for
 import requests
 
@@ -27,4 +14,3 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
     test()
->>>>>>> 55b4197d2492d6d524967200dd42902b1eb96bab
