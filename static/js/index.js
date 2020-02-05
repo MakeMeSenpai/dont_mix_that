70f6224ae689a873551a57284619ed5b8d437367
liyaSileshi <liyasileshi@gmail.com>
2020-02-05 14:52:23 -0800

    working on functionality

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@ let elt1 = document.getElementById('elt1')
 let elt2 = document.getElementById('elt2')
 let elt3 = document.getElementById('elt3')
 
-
+console.log(elt1.style.backgroundColor)
 function el1() {
     elt1.style.backgroundColor = "blue";
     // document.getElementById("demo1").style.visibility="hidden";
@@ -15,7 +15,21 @@ function el1() {
   function el3() {
     elt3.style.backgroundColor = "blue";
   }
+  console.log('**************')
+  const style = getComputedStyle(elt1)
+//   console.log(style)
+  console.log(elt1.style.backgroundColor)
 
+  function change_color(){
+    if (elt1.style.backgroundColor == 'blue'){
+        console.log("don't mix that");
+        alert("game over")
+      }
+      else{
+        console.log("no");
+        //   alert('no')
+      }
+  }
 
 // console.log(elts)
 // function allowDrop(ev) {
@@ -39,4 +53,7 @@ function el1() {
     // var data = ev.dataTransfer.getData("Text");
     // ev.target.appendChild(document.getElementById(data));
 //   }
-  
+
+setInterval(change_color(), 10);
+// setInterval()
+//   change_color()
