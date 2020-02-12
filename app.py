b15b951c71d858242abde2eced10ae13b3e439fc
liyaSileshi <liyasileshi@gmail.com>
2020-02-12 11:46:15 -0800

    added index.html for the homepage and buttons

--- app.py
+++ app.py
@@ -3,14 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-def test():
-  print("action page test")
-
 @app.route('/')
 def home():
-    #Home
-    return render_template('home.html')
+  return render_template('index.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-    test()
