e4edfd2b0dd17315c42a6df6ee039e7717bc3178
Anthony <sean.a.protho1@gmail.com>
2020-02-13 00:25:39 -0800

    bookboi

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 from random import randint
+from test import test_concept
 
-#mongodb
+
+#mongodb imports
 import requests
 from pymongo import MongoClient
 import json
@@ -9,71 +11,44 @@ from bson.objectid import ObjectId
 import os
 
 
+#host for mongodb -link to database
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/dontmixthat')
+#makes our user into a client
 client = MongoClient(host=f'{host}')
+#grabs our database for our client
 db = client.get_default_database()
+#from database grabs our collections
 dontmixthat = db.dontmixthat
 recs = db.recipes
-# host = os.environ.get('MONGODB_URI', 'mongodb://MakeMeSenpai:password1@ds233268.mlab.com:33268/heroku_3jndmb7c')
-# client = MongoClient(host=f'{host}?retryWrites=false')
-# db = client.get_default_database()
-# shop_carts = db.shop_carts
+
 
 app = Flask(__name__)
 
+
+""" test routes"""
 @app.route('/test')
 def test():
-    z = randint(0, 12) * 2.56
-    if z > 20:
-        item1 = "bleach"
-    elif z > 15:
-        item1 = "salt"
-    elif z > 10:
-        item1 = "dirt"
-    else:
-        item1 = "water"
-
-    z = randint(0, 12) * 2.56
-    if z > 20:
-        item2 = "bleach"
-    elif z > 15:
-        item2 = "salt"
-    elif z > 10:
-        item2 = "dirt"
-    else:
-        item2 = "water"
-
-    if item1 == "bleach" and item2 == "bleach":
-        prod = "Super Bleach"
-    elif item1 == "bleach" and item2 == "salt":
-        prod = "Redneck's Weedkiller"
-    elif item1 == "salt" and item2 == "salt":
-        prod = "Super Salt"
-    elif item1 == "bleach" and item2 == "dirt":
-        prod = "Clean Dirt"
-    elif item1 == "dirt" and item2 == "salt":
-        prod = "Super Dirt"
-    elif item1 == "bleach" and item2 == "water":
-        prod = "Mystery Milk"
-    elif item1 == "bleach" and item2 == "salt":
-        prod = "Lake"
-    elif item1 == "dirt" and item2 == "salt":
-        prod = "Slug Trap"
-    elif item1 == "water" and item2 == "salt":
-        prod = "Salt Water"
-    elif item1 == "dirt" and item2 == "water":
-        prod = "Mud"
-    else:
-        return "<h1>Don't Mix That!</h1>" + "<h3>You exploded -trying to mix the unmixable!<h3>"
-    return "<h1>Don't Mix That!</h1>" + f"<h3>You mixed <i>{item1}</i> and <i>{item2}</i>, and got <i>{prod}</i></h3>"
+    # Test - tests combination concept
+    test_concept()
 
 @app.route('/mix')
 def mix():
-    #Mix
+    #Mix - tests mongo
     recipes = recs.find()
     ingredient = dontmixthat.find()
     return render_template('mix.html', ingredients=ingredient, recipes=recipes)
 
+@app.route('/book', method=['POST', 'GET'])
+def book():
+    #Book - tests our recipies, (should be cleared every run)
+    unlocked = []
+    recipes = recs.find()
+    # if mix in recipe (in this case recipe in recipe), if not in unlocked, append unlocked, display unlocked
+    return render_template('book.html', recipes=recipes, unlocked=unlocked)
+
+
+
+"""our main routes"""
 @app.route('/')
 def home():
     #Home
@@ -86,4 +61,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
-    test()
